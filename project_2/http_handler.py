271debb8092f9ebe817ed1910ccfed84fc957bcd
Rofael Aleezada <me@rofael.com>
2018-09-21 22:49:38 -0500

    Fixed 403 caused by sending POST in chunks

--- project_2/http_handler.py
+++ project_2/http_handler.py
@@ -3,7 +3,7 @@ import socket
 
 # HttpHandler used primarily to keep track of cookies
 class HttpHandler:
-    def __init__(self, debug):
+    def __init__(self, debug=False):
         self.socket = None
 
         self.host = "odin.cs.uab.edu"
@@ -12,6 +12,10 @@ class HttpHandler:
         self.connected = False
 
         self.debug = debug
+        self.referer = None
+
+    def set_debug(self, new_debug):
+        self.debug = new_debug
 
     # Connects to Odin on port 3001
     # Returns true if successful, otherwise false
@@ -46,13 +50,14 @@ class HttpHandler:
                 print("failed to close socket: " + str(e))
             return False
 
+        self.socket = None
         self.connected = False
         return True
 
     # Sends a HTTP request over the connect
     # type = either "GET" or "POST"
     # url = portion of URL AFTER the domain
-    #   i.e. http://odin.cs.uab.edu:3001/fakebook -> /fakebook
+    #   i.e. http://odin.cs.uab.edu:3001/fakebook/ -> /fakebook/
     #   it is the job of the person using this function to remove the domain
     #   and make sure the path exists on http://odin.cs.uab.edu:3001
     #   URL parameters should be passed like "/fakebook/?user=hello&pass=there"
@@ -67,15 +72,22 @@ class HttpHandler:
             body = url[url.find("?")+1:]
             url = url[:url.find("?")]
 
-        request = type + " " + url + " HTTP/1.0\r\n"
+        request = type + " " + url + " HTTP/1.1\r\n"
         # If there are saved cookies, add them to the request
         if len(self.cookies) != 0:
             request += "Cookie: "
             for key, val in self.cookies.items():
                 request += key
                 request += "="
+                request += val
                 request += "; "
+            request = request[:len(request)-2]
             request += "\r\n"
+        if body is not None:
+            request += "Content-Type: application/x-www-form-urlencoded\r\n" # Makes body look pretty in Wireshark
+            request += "Content-Length: " + str(len(body)) + "\r\n" # Will otherwise send in chunks and WSGI no likey
+        if self.referer is not None:
+            request += "Referer: http://odin.cs.uab.edu:3001" + url + "\r\n"
         request += "\r\n"
         if body is not None:
             request += body
@@ -104,6 +116,7 @@ class HttpHandler:
 
         self._close()
         response_lines = response.split("\r\n")
+        self.referer = None
 
         # Handle HTTP Response Codes
         http_status = response_lines[0]
@@ -124,7 +137,9 @@ class HttpHandler:
                         return None
 
                     new_request = redirect_url[redirect_url.find("/"):]
-                    return self.send_request(type, new_request)
+                    # Redirect is always GET
+                    self.referer = url
+                    return self.send_request("GET", new_request)
 
             # Didn't find a Location header
             return None
@@ -144,7 +159,7 @@ class HttpHandler:
         # Check for and store cookies
         for line in response_lines:
             if line.find("Set-Cookie") != -1:
-                cookie_name = line[line.find(":") + 2:line.find("=")]
+                cookie_name = line[line.find(":") + 3:line.find("=")]
                 cookie_value = line[line.find("=") + 1:line.find(";")]
                 self.cookies[cookie_name] = cookie_value
 
@@ -155,22 +170,35 @@ class HttpHandler:
 
 
 if __name__ == "__main__":
-    handler = HttpHandler(debug=True)
+    handler = HttpHandler()
 
     # Test simple get
     home_page = handler.send_request("GET", "/")
     if home_page is not None:
-        print(home_page)
+        # print(home_page)
+        pass
 
     # Test a redirecting page
     login_page = handler.send_request("GET", "/fakebook/")
     if login_page is not None:
-        print(login_page)
+        # print(login_page)
+        pass
 
+    # Get CSRF middleware token from login page
+    token_identifier = "name='csrfmiddlewaretoken' value='"
+    token_start = login_page.find(token_identifier)
+    token = ""
+    if token_start != -1:
+        token = login_page[token_start + len(token_identifier):]
+        token = token[:token.find('\'')]
+        token = "&csrfmiddlewaretoken=" + token
+
+    handler.set_debug(True)
     # Test POST and params
-    # main_menu = handler.send_request("POST", "")
-    # if main_menu is not None:
-        # print(main_menu)
+    main_menu = handler.send_request("POST",
+                                     "/accounts/login/?password=BMWTGME7&username=rofael&next=/fakebook/" + token)
+    if main_menu is not None:
+        print(main_menu)
 
     # Test GET with cookies
     # user_menu = handler.send_request("GET", "")
