746fcf0151a3c691399fba09c8276eb0e724f931
Sam Lazrak <samlazrak@Sams-MacBook-Pro.local>
2018-10-02 18:06:43 -0500

    Finished, added comments.

--- project_2/html_parser.py
+++ project_2/html_parser.py
@@ -1,51 +1,25 @@
-from http_handler import HttpHandler
-
+# USAGE:
+# 1. Import
+# 2. Create new HtmlHandler - handler
+# 3. Pass parseHtml() the html recieved from http_handler
+# 4. Do stuff with the returned array
 
 class HtmlHandler:
-    def __init__(self):
-        self.login_page = None
-        self.main_menu = None
-        self.user_menu = None
+    def parseHtml(self, html):
+        from html.parser import HTMLParser
+        flags = []
 
-    def getHtml(self):
-        handler = HttpHandler(True)
+        class MyHTMLParser(HTMLParser):
+            def handle_data(self, data):
+                if data[0:5] == "FLAG:":
+                    flags.append(data)
 
-        handler.connect()
+        parser = MyHTMLParser()
+        parser.feed(html)
 
-        # Test a get
-        login_page = handler.send_request("GET", "/accounts/login")
-        if login_page is not None:
-            self.login_page = login_page
-            print(self.login_page)
+        return flags
 
-        # Get authenticity token from login page
-        token_identifier = "name=\"authenticity_token\" value=\""
-        token_start = login_page.find(token_identifier)
-        token = ""
-        if token_start != -1:
-            auth_token = login_page[token_start + len(token_identifier):]
-            auth_token = auth_token[:auth_token.find("\"")]
-            token += "&authenticity_token=" + auth_token
-
-        # Test POST and params
-        main_menu = None
-        if token != "":
-            main_menu = handler.send_request("POST",
-                                             "/accounts/login/?login[password]=5HRYDLKA&login[email]=lazrak13" + token)
-        if main_menu is not None:
-            self.main_menu = main_menu
-            print(self.main_menu)
-
-        # Test GET with cookies
-        if main_menu is not None:
-            user_menu = handler.send_request("GET", "/fakebook/156714994/")
-            if user_menu is not None:
-                self.user_menu = user_menu
-                print(self.user_menu)
-
-        handler.close()
-
-
+# Tests
 if __name__ == "__main__":
     handler = HtmlHandler()
-    handler.getHtml()
+    print(handler.parseHtml('<h3>FLAG: 1234</h3><h3>FLAG: 12345678</h3><h1>NOPE</h1>'))
