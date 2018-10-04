0309ca09ff5a18b8ec85ace403d08f33e5ec77f1
Sam Lazrak <samlazrak@Sams-MacBook-Pro.local>
2018-10-04 14:38:00 -0500

    added flag finder

--- project_2/html_parser.py
+++ project_2/html_parser.py
@@ -4,16 +4,32 @@ class HtmlHandler:
         soup = BeautifulSoup(html, 'html.parser')
 
         links = []
+        flags = []
 
         for link in soup.find_all('a', href=True):
             links.append(link['href'])
+        for line in soup.find_all('h3',attrs={"class" : "secret_flag"}):
+            flags.append(line.text)
         
-        return links
+        return links, flags
 
 # Tests
 if __name__ == "__main__":
     html = """
+        <html>
+            <body>
+                <p>I'm a paragraph</p>
+                <h3 class='secret_flag'>FLAG: 12klj3123jn1kj321</h3>
+                <h3 class='secret_flag'>FLAG: 1293891023j21i31j2nnkj</h3>
+                <a href="http://example.com"/>
+                <a href="https://example.com"/>
+            </body>
+        </html>
     """
 
     handler = HtmlHandler()
-    print(handler.parseHtml(html))
+    parsed = handler.parseHtml(html)
+    # Print both returned arrays
+    print(parsed)
+    # Accessing the first array [0] ( the links ), at the 2nd position [1]
+    print(parsed[0][1])
