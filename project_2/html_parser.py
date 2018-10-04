fda9ab5dac5ab5dd593d1710225cb9d07b2ddb14
Sam Lazrak <samlazrak@Sams-MacBook-Pro.local>
2018-10-04 08:59:27 -0500

    added link functionality

--- project_2/html_parser.py
+++ project_2/html_parser.py
@@ -8,18 +8,26 @@ class HtmlHandler:
     def parseHtml(self, html):
         from html.parser import HTMLParser
         flags = []
+        links = []
 
         class MyHTMLParser(HTMLParser):
+            def handle_starttag(self, tag, attrs):
+                if tag == 'a':
+                    links.append(attrs)
+                    
             def handle_data(self, data):
+                print(data)
                 if data[0:5] == "FLAG:":
                     flags.append(data)
+                if data[0:4] == "http":
+                    links.append(data)
 
         parser = MyHTMLParser()
         parser.feed(html)
 
-        return flags
+        return flags, links
 
 # Tests
 if __name__ == "__main__":
     handler = HtmlHandler()
-    print(handler.parseHtml('<h3>FLAG: 1234</h3><h3>FLAG: 12345678</h3><h1>NOPE</h1>'))
+    print(handler.parseHtml('<h3>FLAG: 1234</h3><h3>FLAG: 12345678</h3><h1>No flag</h1><a href="https://www.uab.edu/">UAB</a><a href="https://www.uab2.edu/">UAB2</a>'))
